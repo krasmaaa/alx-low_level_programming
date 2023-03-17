462d3289f5e86a7763ab91a97f4ba4279c17d425
krasmaa <krasmaa20@gmail.com>
2023-03-17 06:25:05 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int alb;
+for (alb = '122' ; alb > '96' ; alb--)
+{
+putchar(alb);
+}
+putchar('\n');
+return (0);
+}
