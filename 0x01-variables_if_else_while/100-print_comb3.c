83f9c862f095a8202c2a4a5a6da5699b5726a6fa
krasmaa <krasmaa20@gmail.com>
2023-03-17 06:46:25 -0700

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+int b;
+for (a = 0 ; a < 9 ; a++)
+{
+for (b = 0 ; b < 10 ; b++)
+{
+printf("%d%d", a, b);
+}
+}
+return (0);
+}
