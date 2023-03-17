bef49ccb79096bae1a4315637c50478ab4c883fd
krasmaa <krasmaa20@gmail.com>
2023-03-17 03:28:30 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+/*
+ * main - print aline of using puts
+ *
+ * return:0
+ */
+int main(void)
+{
+	 puts("\n"Programming is like building a multilingual puzzle"");
+	 return (0);
 }
