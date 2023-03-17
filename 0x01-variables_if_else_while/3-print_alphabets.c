de99942fbfcaefde580ce0d222490c22da646692
krasmaa <krasmaa20@gmail.com>
2023-03-17 05:51:19 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 71;
+for (a = 71 ; a < 123 ; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
