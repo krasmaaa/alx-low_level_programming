23726d052dfe93d92175dbef257684ee0ff5e88c
krasmaa <krasmaa20@gmail.com>
2023-03-21 06:58:58 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+void  print_alphabet(void)
+{
+char lettre;
+for (lettre='a' ; lettre <= 'z' ; lettre++)
+{
+_putchar(lettre);
+}
+_putchar('\n');
+}
